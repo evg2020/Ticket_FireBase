9e68e79d3bcff9c1105045cfbb83ea5bf3694ecd
Evgeny Fedorenko <evgeny.fedorenko@constant.obninsk.ru>
2022-05-26 18:09:22 +0300

    fix: commit3

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,39 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp({
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+firebase.initializeApp({
+  messagingSenderId: '448358493027'
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = getMessaging(firebaseApp);
+const messaging = firebase.messaging();
+
+// Customize notification handler
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('Handling background message', payload);
+
+  // Copy data object to get parameters in the click handler
+  payload.data.data = JSON.parse(JSON.stringify(payload.data));
+
+  return self.registration.showNotification(payload.data.title, payload.data);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  const target = event.notification.data.click_action || '/';
+  event.notification.close();
+
+  // This looks to see if the current is already open and focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true
+  }).then(function(clientList) {
+    // clientList always is empty?!
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url === target && 'focus' in client) {
+        return client.focus();
+      }
+    }
+
+    return clients.openWindow(target);
+  }));
+});
